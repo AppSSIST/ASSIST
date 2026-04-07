51c052fb121b25623aa34d06c89e995170bfcc5f
Summer077 <msnbaldonado.tip.edu.ph>
2026-04-07 21:11:19 +0800

    Use SMTP email backend in production for live password reset functionality

--- ASSIST/settings.py
+++ ASSIST/settings.py
@@ -176,8 +176,12 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
-# Email configuration: requires .env file
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' if config('FORCE_SMTP_EMAIL', default=False, cast=bool) else 'django.core.mail.backends.console.EmailBackend'
+# Email configuration: use console backend in development, SMTP in production
+if DEBUG:
+    EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
+else:
+    EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
